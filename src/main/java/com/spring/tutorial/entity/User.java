14ee9ae5f2c069b2cb4304c68f3455d50109559f
PipocaPorto <jpportocampos@gmail.com>
2022-09-13 15:22:52 -0300

    Adding UserController
    Adding UserController with GetAll, Get and Post User

--- src/main/java/com/spring/tutorial/entity/User.java
+++ src/main/java/com/spring/tutorial/entity/User.java
@@ -14,6 +14,12 @@ public class User {
     private long id;
     private String name;
     
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
